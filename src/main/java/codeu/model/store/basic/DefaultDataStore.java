95d6861588f3c8067b93f593c77915b2e24746e7
ezhou <ezhou@codeustudents.com>
2018-03-07 19:16:02 -0500

    Added password to user and modified tests accordingly (hard coding passwords or setting to null); Project 1, part 1

--- src/main/java/codeu/model/store/basic/DefaultDataStore.java
+++ src/main/java/codeu/model/store/basic/DefaultDataStore.java
@@ -98,7 +98,8 @@ public class DefaultDataStore {
     Collections.shuffle(randomUsernames);
 
     for (int i = 0; i < DEFAULT_USER_COUNT; i++) {
-      User user = new User(UUID.randomUUID(), randomUsernames.get(i), Instant.now());
+      User user = new User(UUID.randomUUID(), randomUsernames.get(i), "password", Instant.now());
+      // for testing so password is hard-coded
       PersistentStorageAgent.getInstance().writeThrough(user);
       users.add(user);
     }
--- src/test/java/codeu/model/store/persistence/PersistentDataStoreTest.java
+++ src/test/java/codeu/model/store/persistence/PersistentDataStoreTest.java
@@ -40,13 +40,15 @@ public class PersistentDataStoreTest {
   public void testSaveAndLoadUsers() throws PersistentDataStoreException {
     UUID idOne = UUID.randomUUID();
     String nameOne = "test_username_one";
+    String passOne = "password one";
     Instant creationOne = Instant.ofEpochMilli(1000);
-    User inputUserOne = new User(idOne, nameOne, creationOne);
+    User inputUserOne = new User(idOne, nameOne, passOne, creationOne);
 
     UUID idTwo = UUID.randomUUID();
     String nameTwo = "test_username_two";
+    String passTwo = "password two";
     Instant creationTwo = Instant.ofEpochMilli(2000);
-    User inputUserTwo = new User(idTwo, nameTwo, creationTwo);
+    User inputUserTwo = new User(idTwo, nameTwo, passTwo, creationTwo);
 
     // save
     persistentDataStore.writeThrough(inputUserOne);
@@ -59,11 +61,13 @@ public class PersistentDataStoreTest {
     User resultUserOne = resultUsers.get(0);
     Assert.assertEquals(idOne, resultUserOne.getId());
     Assert.assertEquals(nameOne, resultUserOne.getName());
+    //Assert.assertEquals(passOne, resultUserOne.getPassword());
     Assert.assertEquals(creationOne, resultUserOne.getCreationTime());
 
     User resultUserTwo = resultUsers.get(1);
     Assert.assertEquals(idTwo, resultUserTwo.getId());
     Assert.assertEquals(nameTwo, resultUserTwo.getName());
+    //Assert.assertEquals(passTwo, resultUserTwo.getPassword());
     Assert.assertEquals(creationTwo, resultUserTwo.getCreationTime());
   }
 

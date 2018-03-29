b1d6a62b9e991b8ff8b65f07fe82db69ead0d55f
ezhou <ezhou@codeustudents.com>
2018-03-29 01:46:59 -0400

    Adding profile page servlet (UsersServlet), added to web.xml, created .jsp, and created test for servlet although it has no tests currently

---
+++ src/main/java/codeu/controller/UsersServlet.java
@@ -0,0 +1,138 @@
+package codeu.controller;
+
+import codeu.model.data.Conversation;
+import codeu.model.data.Message;
+import codeu.model.data.User;
+import codeu.model.store.basic.ConversationStore;
+import codeu.model.store.basic.MessageStore;
+import codeu.model.store.basic.UserStore;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+/** Servlet class responsible for the users profile pages. */
+public class UsersServlet extends HttpServlet {
+
+  /** Store class that gives access to Conversations. */
+  private ConversationStore conversationStore;
+
+  /** Store class that gives access to Messages. */
+  private MessageStore messageStore;
+
+  /** Store class that gives access to Users. */
+  private UserStore userStore;
+
+  /** Set up state for handling chat requests. */
+  @Override
+  public void init() throws ServletException {
+    super.init();
+    //setConversationStore(ConversationStore.getInstance());
+    //setMessageStore(MessageStore.getInstance());
+    setUserStore(UserStore.getInstance());
+  }
+
+  /**
+   * Sets the ConversationStore used by this servlet. This function provides a common setup method
+   * for use by the test framework or the servlet's init() function.
+   */
+  /*void setConversationStore(ConversationStore conversationStore) {
+    this.conversationStore = conversationStore;
+  } */
+
+  /**
+   * Sets the MessageStore used by this servlet. This function provides a common setup method for
+   * use by the test framework or the servlet's init() function.
+   */
+  /*void setMessageStore(MessageStore messageStore) {
+    this.messageStore = messageStore;
+  } */
+
+  /**
+   * Sets the UserStore used by this servlet. This function provides a common setup method for use
+   * by the test framework or the servlet's init() function.
+   */
+  void setUserStore(UserStore userStore) {
+    this.userStore = userStore;
+  }
+
+  /**
+   * This function fires when a user navigates to the chat page. It gets the conversation title from
+   * the URL, finds the corresponding Conversation, and fetches the messages in that Conversation.
+   * It then forwards to chat.jsp for rendering.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    String requestUrl = request.getRequestURI();
+    String userName = requestUrl.substring("/users/".length());
+
+	// if the user is viewing their own profile page
+    if (userName == (String) request.getSession().getAttribute("user")) { 
+      System.out.println("" + userName + " has editing permissions!");
+    }
+
+    //request.setAttribute("conversation", conversation);
+    //request.setAttribute("messages", messages);
+    request.getRequestDispatcher("/WEB-INF/view/users.jsp").forward(request, response);
+  }
+
+  /**
+   * This function fires when a user submits the form on the chat page. It gets the logged-in
+   * username from the session, the conversation title from the URL, and the chat message from the
+   * submitted form data. It creates a new Message from that data, adds it to the model, and then
+   * redirects back to the chat page.
+   */
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+
+    String username = (String) request.getSession().getAttribute("user");
+    /*if (username == null) {
+      // user is not logged in, don't let them add a message
+      response.sendRedirect("/login");
+      return;
+    }
+
+    User user = userStore.getUser(username);
+    if (user == null) {
+      // user was not found, don't let them add a message
+      response.sendRedirect("/login");
+      return;
+    }
+
+    String requestUrl = request.getRequestURI();
+    String conversationTitle = requestUrl.substring("/chat/".length());
+
+    Conversation conversation = conversationStore.getConversationWithTitle(conversationTitle);
+    if (conversation == null) {
+      // couldn't find conversation, redirect to conversation list
+      response.sendRedirect("/conversations");
+      return;
+    }
+
+    String messageContent = request.getParameter("message");
+
+    // this removes any HTML from the message content
+    String cleanedMessageContent = Jsoup.clean(messageContent, Whitelist.none());
+
+    Message message =
+        new Message(
+            UUID.randomUUID(),
+            conversation.getId(),
+            user.getId(),
+            cleanedMessageContent,
+            Instant.now());
+
+    messageStore.addMessage(message);*/
+
+    // redirect to a GET request
+    response.sendRedirect("/users/" + username);
+  }
+}
